301528f544a8a483f71c770f202abf68d3a7b196
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-07-29 16:38:34 +0200

    path, cors, dotenv, bodyparser, notes route

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const notesRoute = require("./route/notes");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+);
+
+app.use(bodyParser.json());
+
+var whitelist = ["http://localhost:3000", "http://localhost:3003"];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use("/notes", cors(corsOptions), notesRoute);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
