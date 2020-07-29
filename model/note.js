8f549946bfcaf781958b7604eb15332a8a1be4ff
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-07-29 17:28:59 +0200

    note model and basic routes

---
+++ model/note.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var noteSchema = new Schema(
+  {
+    note: String,
+  },
+  { timestamps: true, collection: "note" }
+);
+
+const noteModel = mongoose.model("note", noteSchema);
+
+module.exports = noteModel;
--- route/notes.js
+++ route/notes.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const noteModel = require("../model/note");
 
 router.get("/", async (req, res) => {
-  res.send("Hello");
+  res.json(await noteModel.find());
+});
+
+router.get("/:_id", async (req, res) => {
+  res.json(await noteModel.findById(req.params._id));
+});
+
+router.post("/", async (req, res) => {
+  const { note } = req.body;
+  const newNote = await noteModel.create({ note });
+  res.json(newNote);
+});
+
+router.put("/:_id", async (req, res) => {
+  const { note } = req.body;
+  const updatedNote = await noteModel.findByIdAndUpdate(
+    req.params._id,
+    {
+      note,
+    },
+    { new: true }
+  );
+  res.send(updatedNote);
+});
+
+router.delete("/:_id", async (req, res) => {
+  await noteModel.findByIdAndDelete(req.params._id);
+  res.send("Note successfully Deleted!");
 });
 
 module.exports = router;
