c3a3ea7416d78cf8e8245aebb07524ccfed77508
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-07-30 02:54:24 +0200

    New build

---
+++ build/precache-manifest.b366b3acc92526c4279c3d222986e174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c7d1f67fdfdeacd94677c50d7cff92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2b32051db151a62eab5",
+    "url": "./static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "e2b32051db151a62eab5",
+    "url": "./static/js/2.cbd5b6df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cbd5b6df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75aea5c758b8e2b8fbfa",
+    "url": "./static/js/main.728f8bc6.chunk.js"
+  },
+  {
+    "revision": "19cebdfb96df36db0c9d",
+    "url": "./static/js/runtime-main.7329239c.js"
+  }
+]);
